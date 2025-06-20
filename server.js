5b91cec202b38cca3a777e77c4e4df7021fcf447
Nicoalazar <nicozalazar67@gmail.com>
2025-06-20 13:21:41 -0300

    ejercicio 10

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 import express from 'express'
 const app = express();
 
-app.get("/ping",(req,res)=> {
-    res.send("/pong")
+const list = [
+    {id:1, name: 'Juan', email: 'juan@gmail.com'},
+    {id:2, name: 'Pedro', email: 'pedro@gmail.com'},
+    {id:3, name: 'María', email: 'maria@gmail.com'},
+    {id:4, name: 'Ana', email: 'ana@gmail.com'},
+    {id:5, name: 'Luis', email: 'luis@gmail.com'}
+]
+
+app.get("/welcome",(req,res)=> {
+    res.send(`
+    <h1> Bienvenido a mi servidor </h1>
+    `)
+})
+
+app.get("/list",(req,res)=> {
+    res.json(list)
 })
 
 const PORT =  3000; 
